45443a6bcf7cde1a02128e114aa1d2ce720e90bd
Jon Waldow <jwaldow@mines.edu>
2012-01-23 20:51:05 -0700

    finishing with lvel 5, 2 more to win

--- project02/rubywarrior/blaze-beginner/player.rb
+++ project02/rubywarrior/blaze-beginner/player.rb
@@ -3,12 +3,19 @@ class Player
     # add your code here
 	if warrior.feel.empty?
 		if warrior.health < 20
-			warrior.rest!
-		else	
+			if warrior.health >= @health
+				warrior.rest!
+			else 
+				warrior.walk!
+			end
+		elsif warrior.feel.captive?
+			warrior.rescue!
+		else
 			warrior.walk!
 		end
 	else
 		warrior.attack!
-	end	
+	end
+	@health = warrior.health
   end
 end
