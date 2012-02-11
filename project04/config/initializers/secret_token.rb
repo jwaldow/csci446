1ed065f69d117c4ce48f6d13b5d3153f3c497dd4
Jon Waldow <jwaldow@mines.edu>
2012-02-11 16:31:46 -0700

    adding inital files

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = 'eb3c8146cef710894b87d5be1a52c9c87969dd1aa1c2d4efe51c65f578e5cb5c908e298db8512655f9f2f4dec715451556cbc886f4d7e6d4ea0cd5a6043ce5b9'
