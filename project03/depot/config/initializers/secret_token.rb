c3e30a5134c2d66769b1163d2fe6fc3956106cb1
Jon Waldow <jwaldow@mines.edu>
2012-02-11 16:59:53 -0700

    just generated the articles scaffold, migrated the table, and point localhost:3000 to that page

--- project03/depot/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '146d2f87805f2f9f3a686e08eba0262535344ecf9267e6c836ae101934947fa7c3e47c621f7274b3f199dcd089f7b054a37e494603fe2e5b62068957260058fd'
--- project04/config/routes.rb
+++ project04/config/routes.rb
@@ -1,4 +1,7 @@
 Articles::Application.routes.draw do
+  get "articles/index"
+  resources :articles
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +52,7 @@ Articles::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'articles#index', as: 'articles'
 
   # See how all your routes lay out with "rake routes"
 
